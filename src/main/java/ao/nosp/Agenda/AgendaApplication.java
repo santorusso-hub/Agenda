0e5ac160c4ab05ca3902b51e486097b933da5c71
Santo André Bai <177656946+santorusso-hub@users.noreply.github.com>
2024-10-17 01:14:20 +0100

    Teste

--- src/main/java/ao/nosp/Agenda/AgendaApplication.java
+++ src/main/java/ao/nosp/Agenda/AgendaApplication.java
@@ -8,7 +8,6 @@ public class AgendaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AgendaApplication.class, args);
-		System.out.println("Hello World!!!");
 	}
 
 }
